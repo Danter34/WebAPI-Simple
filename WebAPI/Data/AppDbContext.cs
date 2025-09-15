284a4af56466e0064a01ecbdb39a23a4784e2e98
Danter34 <atushidaiki@gmail.com>
2025-09-15 15:04:09 +0700

    Done Part 2

---
+++ WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models.Domain;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions): base(dbContextOptions) 
+        {
+
+        }
+        // Define C# model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // You can define the relationships between tables using the Fluent API
+            modelBuilder.Entity<Book_Authors>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.Book_Author)
+                .HasForeignKey(bi => bi.BookId);
+
+            modelBuilder.Entity<Book_Authors>()
+                .HasOne(b => b.Author)
+                .WithMany(ba => ba.Book_Author)
+                .HasForeignKey(bi => bi.AuthorId);
+        }
+
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Authors> Authors { get; set; }
+        public DbSet<Book_Authors> Books_Authors { get; set; }
+        public DbSet<Publishers> Publishers { get; set; }
+    }
+}
---
+++ WebAPI/Models/Domain/Book_Authors.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WebAPI.Models.Domain
+{
+    public class Book_Authors
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        //navigation Properties - One book has many book_author
+        public Books Book { get; set; }
+        public int AuthorId { get; set; }
+        //navigation properties- One author has many book_author
+        public Authors Author { get; set; }
+    }
+}
